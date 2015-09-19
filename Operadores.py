ccadfd534a5f8f96ae277839002ba34c4d9e222e
Jose Antonio Mora <j_antonio_mora@hotmail.com>
2015-09-19 10:05:06 -0500

    Ejemplos

---
+++ Operadores.py
@@ -0,0 +1,30 @@
+# e = 5  Esto es un entero
+# e = 5L Esto es un entero largo
+
+# real = 0.569 #Variable real
+real = 0.26e-3 #Variable con base10
+
+# print (real)
+
+a = 26
+b = 12.3
+c = 3
+d = 4.2
+
+suma = a + b
+print(suma)
+
+resta = a - c
+print(resta)
+
+division = c / a
+print(division)
+
+multi = b * d
+print(multi)
+
+exponente = c ** 2
+print(exponente)
+
+modulo = c % a
+print(modulo)
