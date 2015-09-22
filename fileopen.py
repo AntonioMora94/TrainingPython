ad6e901be1cdebe56c0623fae64c9d27557e524c
Jose Antonio Mora <j_antonio_mora@hotmail.com>
2015-09-22 14:17:27 -0500

    Codigo Comentado

--- fileopen.py
+++ fileopen.py
@@ -7,15 +7,24 @@ def leerTweets():
     #'a' Abre el archivo para escribir. Se crea si el archivo no existe. Solo podemos agregar datos al final 
     
     archivo = open('Tweets.txt','r')
+    
+    #Le quito el salto de linea que pone el txt con el rstrip()
     linea = archivo.readline().rstrip('\n')
     
+    #Contador para las lineas
     nLinea = 1;        
             
+    #Mientras la linea contenga texto
     while linea!="":
-        palabras = linea.split()
-        print nLinea,' -> (', linea, ') contiene ', len(linea), ' caracteres y ', len(palabras), ' palabras'
+        #Separo por palabras cada linea, por defecto el separador es el espacio
+        palabras = linea.split()        
+        print nLinea,' -> (', linea, ') contiene ', len(linea), ' caracteres y ', len(palabras), ' palabras'        
+        #Con la funcion len() conocemos la cantidad que contiene un string                
         linea=archivo.readline().rstrip('\n')
         nLinea = nLinea + 1
+        
+    #Se cierra el archivo
     archivo.close()
     
+#Ejecuto la funcion
 leerTweets()
